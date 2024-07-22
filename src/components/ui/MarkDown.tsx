939a4724c81afe42f353ff674815eca977a21175
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2024-07-22 11:59:17 +0500

    fixed

--- src/components/ui/MarkDown.tsx
+++ src/components/ui/MarkDown.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { CodeProps } from "react-markdown";
+
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface FormattedTextProps {
   text: string;
@@ -11,7 +12,7 @@ const FormattedText: React.FC<FormattedTextProps> = ({ text }) => {
   return (
     <ReactMarkdown
       components={{
-        code({ className, children, ...props }) {
+        code({ className, children, ...props }: CodeProps) {
           const match = /language-(\w+)/.exec(className || "");
           return match ? (
             <SyntaxHighlighter
